0bfa4f72115a046076918a9badc85c9a469c5b6b
guillaumeluneau <guillaume.luneau@y-nov.com>
2014-04-03 09:32:52 +0200

    zbra2
    commit qui sert à que dalle :D

--- lib/Panier.c
+++ lib/Panier.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int CreatePanier(Panier monPanier, produit monProduit) {
+int CreatePanier(Panier monPanier, produit monProduit) 
+{
 	int i;
 
 	for (i = 0; i < 50; i++)
 	{
 
-		if(monPanier->Produit[i].id == monProduit.id) {
+		if(monPanier->Produit[i].id == monProduit.id) 
+		{
 			return 0;
 		}
 
 	}
 	for (i = 0; i < 50; i++)
 	{
-		if(monPanier->Produit[i].id == 0) {
+		if(monPanier->Produit[i].id == 0) 
+		{
 			monPanier->Produit[i] = monProduit;
 			return -1;
 		}
